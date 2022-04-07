bb70c49f6a4c7687cb0ecefbd70e08d92af57cb5
Henry Dexter <henrydexter9012@gmail.com>
2022-04-07 15:46:30 -0400

    add| fetch cards data

--- components/Card.js
+++ components/Card.js
@@ -1,48 +1,42 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
-export const Card = ({
-  apiUrl = "https://nextjs.org",
-  apiUrlLabel = "API url",
-  apiImgSource = "/api.",
-}) => {
+export const Card = ({ apiData }) => {
+  const { title, url, logo, description, category, version, added } = apiData;
+
   return (
     <>
       <article className={styles.card}>
         <header>
-          <a target="_blank" rel="noreferrer" href={apiUrl}>
-            {apiUrlLabel}
+          <a target="_blank" rel="noreferrer" href={url}>
+            {title}
           </a>
         </header>
         <section>
-          <div className={styles.panel_logo}>
-            <a target="_blank" rel="noreferrer" href={apiUrl}>
-              {/* <Image src={apiImgSource} alt="Logo" width={90} height={90} /> */}
-              <img src="https://fakeimg.pl/90x90/"></img>
+          <div className={styles.panel_logo} style={{"backgroundColor": logo.backgroundColor}}>
+            <a target="_blank" rel="noreferrer" href={url}>
+              <img src={logo.url} width="90px" height="90px"></img>
             </a>
           </div>
           <div className={styles.panel_description}>
-            <p>
-              dolorem ipsum, quia dolor sit amet consectetur adipisci velit, sed
-              quia non numquam
-            </p>
+            <p>{description}</p>
           </div>
         </section>
         <footer>
           <div className={styles.footer_header}>
-            <span> Footer description </span>
+            <span> {category} </span>
           </div>
           <div className={styles.footer_versions}>
             <div className={styles.footer_date}>
-              <span>2019-05-01 </span>{" "}
+              <span>{added} </span>
             </div>
             <div className={styles.footer_version}>
-              <span>v 2.5.0-pre </span>
+              <span>{version} </span>
             </div>
           </div>
           <div className={styles.footer_action}>
             <div className={styles.download}>
-              <svg width="18px" height="24px" >
+              <svg width="18px" height="24px">
                 <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
               </svg>
             </div>
--- components/CardList.js
+++ components/CardList.js
@@ -1,13 +1,64 @@
 import { Card } from "./Card";
 import styles from "../styles/Home.module.scss";
+import { useState, useEffect } from "react";
+
+const transformApiData = (apiData) => {
+  let preferred = apiData.preferred,
+    api = apiData.versions[preferred],
+    info = api.info,
+    externalDocs = api.externalDocs || {},
+    contact = info.contact || {},
+    externalUrl = externalDocs.url || contact.url,
+    logo = info["x-logo"] || {},
+    added = new Date(apiData.added).toDateString(),
+    title = info.title,
+    url = externalUrl,
+    description = info.description,
+    category = api.info["x-apisguru-categories"],
+    version = preferred,
+    origUrl;
+
+  api.info["x-origin"]
+    ? (origUrl = api.info["x-origin"][0].url)
+    : (origUrl = api.swaggerUrl);
+
+  return {
+    title,
+    url,
+    logo,
+    description,
+    category,
+    version,
+    added,
+  };
+};
 
 export const CardList = () => {
+  const [apis, setApis] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://api.apis.guru/v2/list.json")
+      .then((response) => response.json())
+      .then((json) => Object.values(json).map((api) => transformApiData(api)))
+      .then((data) => {
+        setApis(data);
+        setLoading(false);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <article className={styles.content_list_wrapper}>
       <div className={styles.content_list}>
-        {[...Array(16).keys()].map((i) => (
-          <Card key={i} styles={styles} />
-        ))}
+        {loading ? (
+          <span> Loading .... </span>
+        ) : (
+          apis.map((api, index) => (
+            <Card key={index} styles={styles} apiData={api} />
+          ))
+        )}
       </div>
     </article>
   );
