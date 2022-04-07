80c270799238288adb491cadea07b746f35bd414
Henry Dexter <henrydexter9012@gmail.com>
2022-04-07 18:23:53 -0400

    fix| amount apis text

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
-export const Header = () => {
+export const Header = ({ amount = "1400" }) => {
   return (
     <div className={styles.header}>
       <h1> OpenAPI Directory </h1>
-      <h4> Documentation and Test Consoles for Over 1400 Public APIs</h4>
+      <h4>
+        Documentation and Test Consoles for Over
+        <span> {amount} </span> Public APIs
+      </h4>
       <p>
         Powered by
         <a target="_blank" rel="noreferrer" href="http://apis.guru">
           APIs Guru
           <span className={styles.logo}>
-            <Image src="/logoApp guru.svg" alt="APIs Guru Logo" width={20} height={20} />
+            <Image
+              src="/logoApp guru.svg"
+              alt="APIs Guru Logo"
+              width={20}
+              height={20}
+            />
           </span>
         </a>
       </p>
